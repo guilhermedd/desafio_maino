62ea26db3f1c2225d566ad89154fbb1ddf1ac2c2
Guilherme Diel <guiddiel@gmail.com>
2024-04-16 08:09:51 -0300

    controllers and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
+  devise_for :users
+
+  resources :users
+  resources :comments
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "articles#index"
-end 
+  root "homepage#index"
+end
